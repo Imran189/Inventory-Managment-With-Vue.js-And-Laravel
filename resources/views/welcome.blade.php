9313a06cc4ae8bc0c8044011b92a15b85f2b7b7f
Imran189 <imranimu189@gmail.com>
2022-04-07 15:24:11 +0600

    2nd commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,6 +9,7 @@
   <meta name="author" content="">
   <link href="{{asset('backend/img/logo/vlogo.png')}}" rel="icon">
   <title>Vue | Inventory</title>
+  <link rel="stylesheet" href="{{asset('css/app.css')}}">
   <link href="{{asset('backend/vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css">
   <link href="{{asset('backend/vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet" type="text/css">
   <link href="{{asset('backend/css/ruang-admin.min.css')}}" rel="stylesheet">
@@ -630,7 +631,8 @@
   <script src="{{asset('backend/vendor/jquery-easing/jquery.easing.min.js')}}"></script>
   <script src="{{asset('backend/js/ruang-admin.min.js')}}"></script>
   <script src="{{asset('backend/vendor/chart.js/Chart.min.js')}}"></script>
-  <script src="{{asset('backend/js/demo/chart-area-demo.js')}}"></script>  
+  <script src="{{asset('backend/js/demo/chart-area-demo.js')}}"></script> 
+  <script src="{{asset('js/app.js')}}"></script> 
 </body>
 
 </html>
