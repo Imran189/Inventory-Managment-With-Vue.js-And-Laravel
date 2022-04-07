45007d6e5cfdfd23555fbc788e334d7edf92a6ec
Imran189 <imranimu189@gmail.com>
2022-04-07 21:35:51 +0600

    2nd commit

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -1,13 +1,11 @@
 import { createRouter, createWebHistory } from "vue-router";
-import AppVue from "./components/App.vue";
+
 
 const router = createRouter({
     history:createWebHistory(),
     routes:[
        {
-        path:'/',
-        name:'AppVue',
-        components:AppVue
+        
        }
 
     ]
