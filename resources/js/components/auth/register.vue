9333b25840caaf19c8fc264abe67d1996de2d82a
Imran189 <imranimu189@gmail.com>
2022-04-09 09:28:52 +0600

    3rd commit

---
+++ resources/js/components/auth/register.vue
@@ -0,0 +1,13 @@
+<template lang="">
+    <div>
+        <p>Register Page</p>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+    
+</style>
--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from '../components/pages/Home'
+import loginVue from "../components/auth/login.vue";
 
 const router = createRouter({
     history:createWebHistory(process.env.BASE_URL),
@@ -8,6 +9,11 @@ const router = createRouter({
         path:'/',
         name:'home',
         component:Home
+       },
+       {
+        path:'/login',
+        name:'loginVue',
+        component:loginVue
        }
 
     ]
