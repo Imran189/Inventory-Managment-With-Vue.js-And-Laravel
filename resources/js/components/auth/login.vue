a14a81e2e5b1394342014a83175a853e1aabcec6
Imran189 <imranimu189@gmail.com>
2022-04-10 23:53:41 +0600

    4th commi

--- resources/js/components/auth/login.vue
+++ resources/js/components/auth/login.vue
@@ -9,7 +9,7 @@
                                 <div class="login-form">
                                     <div class="text-center">
                                         <h1 class="h4 text-gray-900 mb-4">
-                                            Login-reg
+                                            Login-re
                                         </h1>
                                     </div>
                                     <form class="user">
