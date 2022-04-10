87fcf8f96e72e8947c6dcd473fccc6347a97738c
Imran189 <imranimu189@gmail.com>
2022-04-10 23:55:08 +0600

    4th commit

--- resources/js/components/auth/login.vue
+++ resources/js/components/auth/login.vue
@@ -9,7 +9,7 @@
                                 <div class="login-form">
                                     <div class="text-center">
                                         <h1 class="h4 text-gray-900 mb-4">
-                                            Login
+                                            Login-lo
                                         </h1>
                                     </div>
                                     <form class="user">
